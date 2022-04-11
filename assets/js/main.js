3ba1127ac974ee8a4b2d701edc415c31776cbf32
Strong10mede <kmayur809@gmail.com>
2022-04-11 15:45:22 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,19 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
-const sections = document.querySelectorAll("section[id]");
+function scrollActive(){
+    const scrollY = window.pageYOffset
 
-function ScrollActive(){
-    const scrollY = window.pageYOffset;
-
-    sections.forEach(current => {
+    sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
-          sectionTop = current.offsetTop -58,
-          sectionId = current.getAttribute("id");
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
 
-          if(scrollY > sectionTop && scrollY <= sectionHeight + sectionTop){
-            document.querySelector(".nav__list a[href*=" + sectionId).classList.add("active__link");
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__list a[href*=' + sectionId + ']').classList.add('active__link')
+        }else{
+            document.querySelector('.nav__list a[href*=' + sectionId + ']').classList.remove('active__link')
         }
-          else{
-            document.querySelector("nav__list a[href*=" +sectionId).classList.remove("active__link");         
-          }       
-    });    
+    })
 }
-
-window.addEventListener("scroll",ScrollActive);
+window.addEventListener('scroll', scrollActive)

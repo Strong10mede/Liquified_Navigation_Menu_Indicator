7536f722e835f8690b7142cf33834f35ac2728f7
Strong10mede <kmayur809@gmail.com>
2022-04-11 15:14:49 +0530

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll(".nav__link");
+
+function ScrollActive(){
+    const scrollY = window.pageXOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop -58,
+          sectionId = current.getAttribute("id");
+
+          if(scrollY > sectionTop && scrollY <= sectionHeight + sectionTop){
+            document.getElementById(sectionId).classList.add("active__link");
+        }
+          else{
+            document.getElementById(sectionId).classList.remove("active__link");         
+          }       
+    });    
+}
+
+window.addEventListener("scroll",ScrollActive);
